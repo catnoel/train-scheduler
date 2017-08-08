d9177f1d70f2faa2e589732f3b5b0e7c44f59696
Catherine Noel <catherinemnoel@gmail.com>
2017-08-08 15:31:30 -0400

    final update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 
-  // Initialize Firebase
+ 
   var config = {
     apiKey: "AIzaSyA_cwxPZCT-oNGwYxeVVFuqSKX3H-LgGbQ",
     authDomain: "homework-7-21c72.firebaseapp.com",
@@ -15,9 +15,8 @@ $("#add-train-btn").on("click", function () {
   firebase.database().ref().push({
         train: $('#train-name-input').val(),
         destination: $('#destination-input').val(),
-        time: $('#time-input').val(),
         frequency: $('#frequency-input').val(),
-        createdAt: firebase.database.ServerValue.TIMESTAMP
+        minutes: $('#minutes-away').append()
     });
 });
 
@@ -25,4 +24,13 @@ firebase.database().ref().on("child_added", function(snapshot){
     var row = '<tr><td>' + snapshot.val().train + '</td><td>' + snapshot.val().destination + 
                         '</td><td>' + snapshot.val().time + '</td><td>' + snapshot.val().frequency + '</td></tr><hr>';
   $("#train-table tbody").append(row);
+});
+
+// use moment.js for time
+$("#add-train-btn").on("click", function () {
+  event.preventDefault();
+  var userTime = $('#time-input').val();
+  firebase.database().ref().push({
+    time: moment(userTime).format('LT')
+  })
 });
